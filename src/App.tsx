99d3e5ce30a182df4d9092c51fc4c35d5909b57d
saile515 <elias.jorgensen2006@gmail.com>
2022-08-10 18:14:24 +0200

    Added copyright

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
 	}
 
 	return (
-		<div className="flex flex-col items-center py-10 sm:px-10 w-full">
+		<div className="flex flex-col items-center pt-10 w-full min-h-screen">
 			<textarea onChange={(event) => setBaseText(event.target.value)} className="bg-gray-200 rounded-md sm:w-96 h-36 w-[90%] resize-none p-2" />
 			<button className="bg-blue-500 text-white font-black p-2 rounded-md my-4" onClick={convert}>
 				Skapa Ordlista
@@ -49,14 +49,20 @@ function App() {
 					<button className="absolute right-0 top-0" onClick={copy}>
 						<img src="/copy.svg" alt="Kopiera" />
 						<p
-							className={`bg-green-500 text-white rounded-full p-1 ml-2 font-bold absolute left-full top-0 w-40 text-sm transition-opacity ease-in-out ${
-								copyAlert ? "opacity-100" : "opacity-0"
+							className={`bg-green-500 text-white rounded-full p-1 mr-2 font-bold absolute right-full top-0 w-40 text-sm transition-opacity ease-in-out ${
+								copyAlert ? "opacity-70" : "opacity-0"
 							}`}>
 							Kopierad till urklipp
 						</p>
 					</button>
 				</div>
 			)}
+			<p className="mt-auto mb-2">
+				&copy; 2022 -{" "}
+				<a className="text-blue-700" href="https://www.eliasjorgensen.se">
+					Elias Jörgensen
+				</a>
+			</p>
 		</div>
 	);
 }
