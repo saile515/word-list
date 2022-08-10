e5094db3be1921dd700817efeb40eb440f4d679d
saile515 <elias.jorgensen2006@gmail.com>
2022-08-10 20:57:31 +0200

    Added title

--- src/App.tsx
+++ src/App.tsx
@@ -33,19 +33,21 @@ function App() {
 	}
 
 	return (
-		<div className="flex flex-col items-center pt-10 w-full min-h-screen">
-			<textarea onChange={(event) => setBaseText(event.target.value)} className="bg-gray-200 rounded-md sm:w-96 h-36 w-[90%] resize-none p-2" />
+		<div className="flex flex-col items-center pt-10 w-full h-screen">
+			<h1 className="text-3xl font-black mb-4">Ordlist-skapare</h1>
+			<p>Klistra in text:</p>
+			<textarea onChange={(event) => setBaseText(event.target.value)} className="bg-gray-200 rounded-md sm:w-96 h-36 w-[90%] resize-none p-2 flex-shrink-0" />
 			<button className="bg-blue-500 text-white font-black p-2 rounded-md my-4" onClick={convert}>
 				Skapa Ordlista
 			</button>
 			{wordList.length > 0 && (
-				<div className="relative sm:w-96 w-[90%]">
-					<ul>
+				<div className="relative sm:w-96 w-[90%] flex flex-shrink overflow-hidden">
+					<ul className="overflow-y-auto w-full p-1">
 						{wordList.map((word, index) => (
 							<Word text={word} removeWord={removeWord} key={index + word} />
 						))}
 					</ul>
-					<button className="absolute right-0 top-0" onClick={copy}>
+					<button className="absolute right-6 top-0" onClick={copy}>
 						<img src="/copy.svg" alt="Kopiera" />
 						<p
 							className={`bg-green-500 text-white rounded-full p-1 mr-2 font-bold absolute right-full top-0 w-40 text-sm transition-opacity ease-in-out ${
