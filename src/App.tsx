21bebff6fec9ad09be3c3e66664b1140be18aa0e
saile515 <elias.jorgensen2006@gmail.com>
2022-08-13 21:47:25 +0200

    Added sort and filter button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,119 @@
 import "./App.css";
 
+import { MouseEvent, useState } from "react";
+
 import Word from "./Word";
-import { useState } from "react";
+
+const filterList = [
+	"i",
+	"och",
+	"att",
+	"det",
+	"som",
+	"en",
+	"på",
+	"är",
+	"av",
+	"för",
+	"med",
+	"till",
+	"den",
+	"har",
+	"de",
+	"inte",
+	"om",
+	"ett",
+	"han",
+	"men",
+	"var",
+	"jag",
+	"sig",
+	"från",
+	"vi",
+	"så",
+	"kan",
+	"man",
+	"när",
+	"år",
+	"säger",
+	"hon",
+	"under",
+	"också",
+	"efter",
+	"eller",
+	"nu",
+	"sin",
+	"där",
+	"vid",
+	"mot",
+	"ska",
+	"skulle",
+	"kommer",
+	"ut",
+	"får",
+	"finns",
+	"vara",
+	"hade",
+	"alla",
+	"andra",
+	"mycket",
+	"än",
+	"här",
+	"då",
+	"bara",
+	"in",
+	"blir",
+];
+
+interface ButtonAlert {
+	visible: boolean;
+	message: string;
+	color: string;
+}
+
+function FunctionButton(props: { callback: (event?: MouseEvent) => any; clickMessage: string; hoverMessage?: string; icon: string }) {
+	const [alert, setAlert] = useState<ButtonAlert>({ visible: false, message: "", color: "blue" });
+	return (
+		<div className="relative items-center mb-2 flex">
+			<button
+				className="sm:hidden bg-gray-700 opacity-50 font-bold text-sm text-white rounded-full w-4 h-4 leading-4 mr-1"
+				onClick={() => {
+					if (props.hoverMessage) {
+						setAlert({ visible: true, message: props.hoverMessage, color: "blue" });
+						setTimeout(() => setAlert({ ...alert, visible: false }), 3000);
+					}
+				}}>
+				?
+			</button>
+			<button
+				onClick={(event) => {
+					setAlert({ visible: true, message: props.clickMessage, color: "green" });
+					setTimeout(() => setAlert({ ...alert, visible: false }), 3000);
+					props.callback(event);
+				}}
+				onMouseEnter={() => {
+					if (props.hoverMessage) setAlert({ visible: true, message: props.hoverMessage, color: "blue" });
+				}}
+				onMouseLeave={() => {
+					if (props.hoverMessage) setAlert({ ...alert, visible: false });
+				}}>
+				<img src={props.icon} alt="Filtrera" className="w-6 h-6" />
+			</button>
+			<p
+				className={`${
+					alert.color == "green" ? "bg-green-500" : "bg-blue-500"
+				} absolute right-full top-0 text-white rounded-full py-1 px-2 mr-2 font-bold whitespace-nowrap text-sm transition-opacity ease-in-out ${
+					alert.visible ? "opacity-90 sm:opacity-70" : "opacity-0"
+				}`}>
+				{alert.message}
+			</p>
+		</div>
+	);
+}
 
 function App() {
 	const [baseText, setBaseText] = useState<string>("");
 	const [wordList, setWordList] = useState<string[]>([]);
-	const [copyAlert, setCopyAlert] = useState<boolean>(false);
 
 	function convert() {
 		const words = baseText
@@ -20,39 +127,46 @@ function App() {
 	}
 
 	function removeWord(word: string) {
-		setWordList(wordList.filter((item) => item.toLowerCase() != word.toLowerCase()));
+		setWordList(wordList.filter((item) => item != word.toLowerCase()));
 	}
 
 	function copy() {
 		navigator.clipboard.writeText(wordList.toString().replaceAll(",", "\n")).then(() => {});
-		setCopyAlert(true);
-		setTimeout(() => setCopyAlert(false), 3000);
+	}
+
+	function filter() {
+		let wordListCopy = [...wordList];
+		filterList.forEach((word) => (wordListCopy = wordListCopy.filter((item) => item != word)));
+		setWordList(wordListCopy);
+	}
+
+	function sort() {
+		setWordList([...wordList].sort());
 	}
 
 	return (
 		<div className="flex flex-col items-center pt-10 w-full h-screen">
 			<h1 className="text-3xl font-black mb-4">Ordlist-skapare</h1>
 			<p>Klistra in text:</p>
-			<textarea onChange={(event) => setBaseText(event.target.value)} className="bg-gray-200 rounded-md sm:w-96 h-36 w-[90%] resize-none p-2 flex-shrink-0" />
+			<textarea
+				onChange={(event) => setBaseText(event.target.value)}
+				className="bg-gray-200 rounded-md sm:w-96 h-36 w-[90%] resize-none p-2 flex-shrink-0"
+			/>
 			<button className="bg-blue-500 text-white font-black p-2 rounded-md my-4" onClick={convert}>
 				Skapa Ordlista
 			</button>
 			{wordList.length > 0 && (
-				<div className="relative sm:w-96 w-[90%] flex flex-shrink overflow-hidden">
+				<div className="relative sm:w-96 w-[90%] min-h-[6rem] flex flex-shrink overflow-hidden">
 					<ul className="overflow-y-auto w-full p-1">
 						{wordList.map((word, index) => (
 							<Word text={word} removeWord={removeWord} key={index + word} />
 						))}
 					</ul>
-					<button className="absolute right-6 top-0" onClick={copy}>
-						<img src="/copy.svg" alt="Kopiera" />
-						<p
-							className={`bg-green-500 text-white rounded-full p-1 mr-2 font-bold absolute right-full top-0 w-40 text-sm transition-opacity ease-in-out ${
-								copyAlert ? "opacity-70" : "opacity-0"
-							}`}>
-							Kopierad till urklipp
-						</p>
-					</button>
+					<div className="absolute right-0 top-0 flex flex-col">
+						<FunctionButton callback={copy} clickMessage="Kopierad till urklipp" hoverMessage="Kopiera" icon="/copy.svg" />
+						<FunctionButton callback={filter} clickMessage="Filtrerade ordlista" hoverMessage="Filtrera vanliga ord" icon="/filter.svg" />
+						<FunctionButton callback={sort} clickMessage="Sorterade ordlista" hoverMessage="Sortera i bokstavsordning" icon="/sort.svg" />
+					</div>
 				</div>
 			)}
 			<p className="mt-auto mb-2">
